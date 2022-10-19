db765b9b6c5db7101de122364ec854fb6ca1244a
EMILY GAVIN - STUDENT <G00382828@atu.ie>
2022-10-19 10:56:35 +0100

    Added new calculator methods

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -4,4 +4,24 @@ public class Calc {
         int total = valueOne + valueTwo;
         return total;
     }
+
+    public static int subtract(int valueOne, int valueTwo) throws Exception {
+        if(valueTwo > valueOne){
+            throw new Exception("Value two is greater than value one.");
+        }
+        else{
+            int total = valueOne - valueTwo;
+            return total;
+        }
+    }
+
+    public static int multiply(int  valueOne, int  valueTwo){
+        int total = valueOne*valueTwo;
+        return total;
+    }
+
+    public static int divide(int  valueOne, int  valueTwo){
+        int total = valueOne/valueTwo;
+        return total;
+    }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -15,4 +15,19 @@ class CalcTest {
     void testAdd(){
         assertEquals(9, calc.add(4, 5));
     }
+
+    @Test
+    void testSubtract() throws Exception {
+        assertEquals(4, calc.subtract(10, 6));
+    }
+
+    @Test
+    void testMultiply(){
+        assertEquals(81, calc.multiply(9,9));
+    }
+
+    @Test
+    void testDivide(){
+        assertEquals(3, calc.divide(9,3));
+    }
 }
